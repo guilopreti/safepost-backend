3b4707d16992712808ae54b31bf305757c00f9bc
guilopreti <guilherme.llopreti@gmail.com>
2026-03-17 17:49:02 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import express from "express";
 import postRoutes from "./routes/postRoutes";
 import metricsRoutes from "./routes/metricsRoutes";
+import cors from "cors";
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://safepost-frontend.vercel.app",
+];
 
 const app = express();
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+  }),
+);
 
 // Middlewares padrão em JSON e suporte a formulários URL-enconded
 app.use(express.json());
